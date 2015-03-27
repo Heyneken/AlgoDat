d89bf3c3d074bd3e2097da7691c65f3260cbe571
Heyneken <heyneken@gmx.de>
2015-03-27 15:17:29 +0100

    SimpleLexer

--- src/SimpleLexer.java
+++ src/SimpleLexer.java
@@ -1,6 +1,8 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.util.StringTokenizer;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * Created by Heyneken on 27.03.15.
@@ -8,9 +10,9 @@ import java.util.StringTokenizer;
 public class SimpleLexer implements ILexer {
 
     final private BufferedReader reader;
-    final private IMapFactory mapFactory = new TreeMapFactory();
-    final private IActionAtInsert actionAtInsert = new StringCoding(4711);
-    final private ITrie trie;
+    final private IMapFactory mapFactory = new TreeMapFactory(); // DIC
+    final private IActionAtInsert actionAtInsert = new StringCoding(4711); // DIC
+    final private ITrie trie; // DIC
     private String line;
     private StringTokenizer tk = null;
 
@@ -18,6 +20,51 @@ public class SimpleLexer implements ILexer {
         this.reader = reader;
         line = reader.readLine();
         if (line != null) tk = new StringTokenizer(line);
-        this.trie = new Trie(mapFactory);
+        this.trie = new Trie(mapFactory); // DIC
+    }
+
+    public IToken getNextToken() throws IOException {
+        Logger.getLogger(SimpleLexer.class.getName()).log(Level.INFO, "--> next token");
+        IToken result = null;
+        boolean foundToken = false;
+        boolean noMoreTaken = false;
+        do // Invariante: Es gibt einen Tokenizer; tk != null;
+        {
+            result = null;
+            if (tk != null) {
+                if (tk.hasMoreElements()) {
+                    String intermediate = tk.nextToken();
+                    Logger.getLogger(SimpleLexer.class.getName()).log(Level.INFO, "--- next token:" + intermediate);
+                    // später: result =
+                    // trie.insert(intermediate, action);
+                    // DIC
+                    result = new Token(-1, -1); // ein Dummy
+                    foundToken = true;
+                }
+                else {
+                    // neue Zeile einlesen
+                    tk = null;
+                    line.reader.readLine();
+                    if (line != null) {
+                        tk = new StringTokenizer(line);
+                    }
+                }
+            }
+            else {
+                noMoreTaken = true;
+            }
+        }
+        while (!foundToken && !noMoreTaken) {
+            Logger.getLogger(SimpleLexer.class.getName()).log(Level.INFO, "<-- result Token: " + result);
+            return result;
+        }
+    }
+
+    public String decode(IToken tk) throws UnsupportedOperationException {
+        throw new UnsupportedOperationException("nochNichtImplementiert");
+    }
+
+    public  String toString() {
+        return "\nResult Trie \n" + trie;
     }
 }
